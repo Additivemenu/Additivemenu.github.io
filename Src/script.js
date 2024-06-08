96791c50cec00abd650ed9e6b5a80ff0b5141324
XUeshuo Li <spoonlee24k@gmail.com>
2024-06-08 16:17:33 +1000

    added some comments

--- Src/script.js
+++ Src/script.js
@@ -1,16 +1,20 @@
-const productContainers = [...document.querySelectorAll('.productCardList_container')];
-const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
-const preBtn = [...document.querySelectorAll('.pre-btn')];
+const productContainers = [
+  ...document.querySelectorAll(".productCardList_container"),
+];
+const nxtBtn = [...document.querySelectorAll(".nxt-btn")];
+const preBtn = [...document.querySelectorAll(".pre-btn")];
 
+// bound scrolling handler 
 productContainers.forEach((item, i) => {
-    let containerDimensions = item.getBoundingClientRect();     // element method
-    let containerWidth = containerDimensions.width;
+  let containerDimensions = item.getBoundingClientRect(); // get the dimensions of the container
+  let containerWidth = containerDimensions.width;
+  // console.log(containerDimensions.width);
 
-    nxtBtn[i].addEventListener('click', () => {
-        item.scrollLeft += containerWidth;          // 每次点击button, scroll的宽度; scrollLeft is an element property
-    })
+  nxtBtn[i].addEventListener("click", () => {
+    item.scrollLeft += containerWidth; // scrolling width when clicking on button,; scrollLeft is an element property
+  });
 
-    preBtn[i].addEventListener('click', () => {
-        item.scrollLeft -= containerWidth;
-    })
-})
+  preBtn[i].addEventListener("click", () => {
+    item.scrollLeft -= containerWidth;
+  });
+});
